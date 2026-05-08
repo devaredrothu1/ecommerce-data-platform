8bd817941e037a2846bcf2180eb3e73223d9d443
Deva_redrothu <devaredrothu@gmail.com>
2026-05-08 11:15:04 +0530

    Create insert_data.sql

---
+++ database/database/insert_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO customers VALUES
+(1,'Ravi','Hyderabad'),
+(2,'Anita','Delhi');
+
+INSERT INTO products VALUES
+(101,'Laptop','Electronics',50000),
+(102,'Shoes','Fashion',3000);
+
+INSERT INTO orders VALUES
+(1001,1,'2024-01-01'),
+(1002,2,'2024-01-02');
+
+INSERT INTO order_items VALUES
+(1,1001,101,1),
+(2,1002,102,2);
